809b7e83104d631b406efbf9b175a78426ceb0b3
Matthew Giannini <Matthew Giannini 1274158+mgiannini@users.noreply.github.com>
2023-08-07 08:50:06 -0400

    xeto: stub NameDict and NameTable for es build

---
+++ src/core/xeto/es/NameDict.js
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2023, Brian Frank
+// All Rights Reserved
+//
+// History:
+//  07 Aug 2023  Matthew Giannini Creation
+//
+
+/**
+ * NameDict
+ */
+class NameDict extends sys.Obj
+{
+  constructor() { super(); }
+
+  typeof$() { return NameDict.type$; }
+}
---
+++ src/core/xeto/es/NameTable.js
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2023, Brian Frank
+// All Rights Reserved
+//
+// History:
+//  07 Aug 2023  Matthew Giannini Creation
+//
+
+/**
+ * NameTable
+ */
+class NameTable extends sys.Obj
+{
+  constructor() { super(); }
+
+  typeof$() { return NamewTable.type$; }
+}
