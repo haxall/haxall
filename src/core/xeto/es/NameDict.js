5403ee16265d29ba10c3d070ae2988ec6946ec85
Matthew Giannini <Matthew Giannini 1274158+mgiannini@users.noreply.github.com>
2023-08-16 16:16:31 -0400

    xeto: implement NameTable and NameDict for ES

--- src/core/xeto/es/NameDict.js
+++ src/core/xeto/es/NameDict.js
@@ -11,7 +11,85 @@
  */
 class NameDict extends sys.Obj
 {
-  constructor() { super(); }
+  constructor(table, spec, ...entries) { 
+    super(); 
+    this.#table = table;
+    this.#spec = spec;
+    this.#entries = sys.Map.make(sys.Int.type$, sys.Obj.type$);
+    this.#entries.ordered(true);
+    let i = 0;
+    while (i < entries.length) {
+      const code = table.add(entries[i]);
+      const val  = entries[i+1];
+      this.#entries.add(code, sys.ObjUtil.toImmutable(val));
+      i += 2;
+    }
+  }
 
-  typeof$() { return NameDict.type$; }
+  static #empty;
+  static empty() {
+    if (!NameDict.#empty) NameDict.#empty = new NameDict(null, null, []);
+    return NameDict.#empty;
+  }
+
+  #table;
+  #spec;
+  #entries;
+
+  typeof() { return NameDict.type$; }
+
+  spec() { return this.#spec ?? XetoEnv.cur().dictSpec(); }
+
+  id() {
+    const val = this.#get(this.#table.idCode())
+    if (val != null) return sys.ObjUtil.as(val, haystack.Ref.type$);
+    throw sys.UnresolvedErr.make("id");
+  }
+
+  isEmpty() { return this.#entries.isEmpty(); }
+
+  size() { return this.#entries.size(); }
+
+  has(name) { return this.get(name) != null; }
+
+  missing(name) { return this.get(name) == null; }
+
+  get(name, def=null) { 
+    return this.#get(this.#table.toCode(name), def);
+  }
+
+  getByCode(code) { return this.#get(code); }
+
+  #get(code, def=null) {
+    return this.#entries.get(code) ?? def;
+  }
+
+  each(f) { this.#entries.each(f); }
+
+  eachWhile(f) { return this.#entries.eachWhile(f); }
+
+  map(f) {
+    const newEntries = [];
+    for (const [key, value] of this.#entries) {
+      const name = this.#table.toName(key);
+      const newVal = f(value, name);
+      newEntries.push(name, newVal);
+    }
+    return new NameDict(this.#table, this.#spec, ...newEntries);
+  }
+
+  fixedSize() { return -1; }
+
+  trap(name, args=null) { 
+    const val = this.get(name);
+    if (val != null) return val;
+    throw sys.UnresolvedErr.make(name);
+  }
+
+  nameAt(i) { return this.#entries.keys().get(i); }
+
+  valAt(i) { return this.#entries.vals().get(i); }
+
+  // if we do toStr() probably should be str name instead of int code for key
+  // toStr() { return this.#entries.toStr(); }
 }
