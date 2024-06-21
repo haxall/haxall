5d7f681b268f9d7490d82850e886f89f528738d2
Rachel Scott <rachelsgcount@gmail.com>
2024-06-21 09:47:48 -0400

    Some renaming, and completion. Finished docker setup includes five files:
    docker/Dockerfile
    docker/docker-compose.yml
    docker/.dockerignore
    docker/hx_dockerstart.sh
        this is the wrapper script.
    
    .github/workflows/publish-dockerdefault.yaml
    
    To build the image, go into the docker folder and run this command,
    or just put the path to docker-compose.yml file after the -f option, if running from a different location.
    
    docker compose -f docker-compose.yml build --pull --no-cache --progress=plain
    
    To test it, do:
    docker compose -f docker-compose.yml run --rm -it -p 8082:8082 haxall

--- docker/hx_dockerstart.sh
+++ docker/hx_dockerstart.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
 EXPORT PATH=$PATH:/app/haxall/bin
+echo "$DB_NAME"
 
-# If "var" is changed, the bind-mount in compose needs to change.
-# Currently the volume's container location is "/app/haxall/var"
-# EX: if "var" is changed to "demo" then the location should be "/app/haxall/demo"
-# This volume is connected via bind-mounts, so is expected to exist on the local system. 
-if [ ! -d "$1" ]; then
-    fan hx init -headless var
+# This volume is connected via bind-mount. The bind-mount, under volumes in the compose file, will create the folder on local system if it doesn't exist. 
+# The bind-mount is /app/haxall/dbs and locally is referred to as "dbs"
+# The following creates the bind-mount folder if it doesn't exist, and cd's into it.
+if [ -d "dbs" ]; then
+    cd dbs
+else
+    mkdir dbs
+    cd dbs
 fi
 
-#needs the file location (i.e.)
-fan hx run var
+
+# The following runs hx init with the given database name from build argument DB_NAME (default is "var").
+# It can be changed when doing docker compose commands like so: --build-arg DB_NAME=example
+# It is stored under an ENV variable of same name (BD_NAME) created during the dockerfile build context, and persists after. 
+if [ -d "$DB_NAME" ]; then
+    # The directory exists, but is EMPTY. 
+    if [ -z "$(ls -A "$DB_NAME")" ]; then
+        echo "$DB_NAME is empty, running hx init..."
+        fan hx init -headless "$DB_NAME"
+    # The directory exists, and is NOT empty.
+    else
+        echo "$DB_NAME already exists. \nIf you want to modify your superuser account or the HTTP port, run the 'fan hx init' command again on the same directory, like so: \nfan hx init $DB_NAME\n"
+    fi
+# The directory does not exist.
+else
+    echo "$DB_NAME does not exist, running hx init..."
+    fan hx init -headless "$DB_NAME"
+fi
+
+# This runs haxall after creating the database. Any errors here are likely caused by the bind-mounted database being edited on the local file system by the host. 
+# Command "hx run" needs the file location of the database (i.e. /app/haxall/dbs/$DB_NAME)
+fan hx run "$DB_NAME"
